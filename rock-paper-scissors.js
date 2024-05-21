3cda65efb9c5f0bc486eff1c854e415dfd043cc0
Marc McCarty <maruku115@gmail.com>
2024-05-21 18:39:10 -0500

    Add logic to get human choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,10 +1,25 @@
 function getComputerChoice() {
     let rng = Math.random();
     if (rng < (1/3)) {
-        return "Rock!";
+        return "CPU: \"Rock!\"";
     } else if (rng < 2/3) {
-        return "Paper!";
+        return "CPU: \"Paper!\"";
     } else {
-        return "Scissors!";
+        return "CPU: \"Scissors!\"";
+    }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Please choose \"Rock\", \"Paper\", or \"Scissors\" by typing your selection below:");
+    let normalizedHumanChoice = humanChoice.replaceAll('"','').trim().toLowerCase();
+    if (normalizedHumanChoice == "rock") {
+        return "You: \"Rock!\"";
+    } else if (normalizedHumanChoice == "paper") {
+        return "You: \"Paper!\"";
+    } else if (normalizedHumanChoice == "scissors") {
+        return "You: \"Scissors!\"";
+    } else {
+        console.log("Your response was: " + humanChoice);
+        console.log("Please type either \"Rock\", \"Paper\", or \"Scissors\" next time.");
     }
 }
