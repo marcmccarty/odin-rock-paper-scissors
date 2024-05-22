310de96f8bbc3a4c49145c6deaaef85a539f7308
Marc McCarty <maruku115@gmail.com>
2024-05-22 12:41:43 -0500

    Add feature to allow quitting the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,7 +11,14 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("Please choose \"Rock\", \"Paper\", or \"Scissors\" by typing your selection below:");
-    let normalizedHumanChoice = humanChoice.replaceAll('"','').trim().toLowerCase();
+    let normalizedHumanChoice;
+    
+    if (humanChoice != null) {
+        normalizedHumanChoice = humanChoice.replaceAll('"','').trim().toLowerCase();
+    } else {
+        return "cancel";
+    }
+    
     if (normalizedHumanChoice == "rock") {
         console.log("You: \"Rock!\"");
         return normalizedHumanChoice;
@@ -27,59 +34,7 @@ function getHumanChoice() {
     }
 }
 
-function playGame() {
-    let computerScore = 0;
-    let humanScore = 0;
-    
-    function playRound(computerChoice = getComputerChoice(), humanChoice = getHumanChoice()) {
-        // CPU: "Rock!"
-        if (computerChoice == "rock") {
-            console.log("CPU: \"Rock!\"");
-            if (humanChoice == "paper") {
-                humanScore += 1;
-                console.log("You win!");
-            } else if (humanChoice == "scissors") {
-                computerScore += 1;
-                console.log("CPU wins!");
-            } else {
-                console.log("It's a draw!");
-            }
-        }
-    
-        // CPU: "Paper!"
-        if (computerChoice == "paper") {
-            console.log("CPU: \"Paper!\"");
-            if (humanChoice == "scissors") {
-                humanScore += 1;
-                console.log("You win!");
-            } else if (humanChoice == "rock") {
-                computerScore += 1;
-                console.log("CPU wins!");
-            } else {
-                console.log("It's a draw!");
-            }
-        }
-    
-        // CPU: "Scissors!"
-        if (computerChoice == "scissors") {
-            console.log("CPU: \"Scissors!\"");
-            if (humanChoice == "rock") {
-                humanScore += 1;
-                console.log("You win!");
-            } else if (humanChoice == "paper") {
-                computerScore += 1;
-                console.log("CPU wins!");
-            } else {
-                console.log("It's a draw!");
-            }
-        }
-    }
-
-    for (i = 1; i <= 5; i++) {
-        console.log("\nRound " + i);
-        playRound();
-    }
-
+function calculateFinalScores(computerScore = 0, humanScore = 0) {
     console.log("\nFinal Scores");
     console.log("CPU: " + computerScore);
     console.log("You: " + humanScore);
@@ -91,4 +46,80 @@ function playGame() {
     } else {
         console.log("\nIt's a draw!")
     }
+}
+
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
+    let cancelStatus = "keep playing";
+    
+    function playRound(computerChoice = getComputerChoice(), humanChoice = getHumanChoice()) {
+        // You hit the Cancel button.
+        if (humanChoice == "cancel") {
+            console.log("You hit the Cancel button. Thank you for playing.");
+            return humanChoice;
+        } else {
+            // CPU: "Rock!"
+            if (computerChoice == "rock") {
+                console.log("CPU: \"Rock!\"");
+                if (humanChoice == "paper") {
+                    humanScore += 1;
+                    console.log("You win!");
+                } else if (humanChoice == "scissors") {
+                    computerScore += 1;
+                    console.log("CPU wins!");
+                } else {
+                    console.log("It's a draw!");
+                }
+            }
+        
+            // CPU: "Paper!"
+            if (computerChoice == "paper") {
+                console.log("CPU: \"Paper!\"");
+                if (humanChoice == "scissors") {
+                    humanScore += 1;
+                    console.log("You win!");
+                } else if (humanChoice == "rock") {
+                    computerScore += 1;
+                    console.log("CPU wins!");
+                } else {
+                    console.log("It's a draw!");
+                }
+            }
+        
+            // CPU: "Scissors!"
+            if (computerChoice == "scissors") {
+                console.log("CPU: \"Scissors!\"");
+                if (humanChoice == "rock") {
+                    humanScore += 1;
+                    console.log("You win!");
+                } else if (humanChoice == "paper") {
+                    computerScore += 1;
+                    console.log("CPU wins!");
+                } else {
+                    console.log("It's a draw!");
+                }
+            }
+
+            return "keep playing"
+        }
+    }
+
+    // Round 1 (Allows user to get out of having to play 5 rounds if they want to quit)
+    console.log("\nRound 1 of 5");
+    cancelStatus = playRound();
+
+    if (cancelStatus != "cancel") {
+        // Rounds 2 -> 5
+        for (i = 2; i <= 5; i++) {
+            if (cancelStatus != "cancel") {
+                console.log("\nRound " + i + " of 5");
+                cancelStatus = playRound();
+            } else {
+                i = 6;
+            }
+        }
+    }
+
+    calculateFinalScores(computerScore, humanScore);
 }
